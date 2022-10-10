0506ee5a628e6dad7484bc2669fe7fb244a2d84d
aknEvrnky <evrenkaya.2002.save@gmail.com>
2022-10-10 17:51:48 +0300

    Added Sort class with insertion algorithm

---
+++ Sort.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Necmi Akin Evrenkaya on 10.10.2022.
+//
+
+#include "Sort.h"
+#include <chrono>
+using namespace std::chrono;
+
+int Sort::Sortable::Insertion(int arr[], int n) {
+    // start timer
+    auto start = high_resolution_clock::now();
+
+    // this is the algorithm which indicated in the book
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+
+    // stop timer
+    auto stop = high_resolution_clock::now();
+
+    // return the duration as microseconds
+    return (duration_cast<microseconds>(stop - start)).count();
+}
---
+++ Sort.h
@@ -0,0 +1,17 @@
+//
+// Created by Necmi Akin Evrenkaya on 10.10.2022.
+//
+
+#ifndef SORTING_SORT_H
+#define SORTING_SORT_H
+
+
+class Sort {
+    class Sortable {
+    public:
+        int static Insertion(int[], int);
+    };
+};
+
+
+#endif //SORTING_SORT_H
