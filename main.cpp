497811fa331d2bf5ff642952623bdd5b5f6dce10
aknEvrnky <evrenkaya.2002.save@gmail.com>
2022-10-10 18:36:08 +0300

    added main functionality to code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include "Sort.h"
+
+int getRandomNumber(int min, int max) {
+    return rand() % (max - min + 1) + min;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int nValue, nSize;
+    std::cout << "Enter the N value: " << std::endl;
+    std::cin >> nValue;
+    std::cout << "Enter the size of the array: " << std::endl;
+    std::cin >> nSize;
+
+    // create 2d dynamic array => it will create nSize arrays with ( nValue * (i+1) ) elements
+    // if our n value is 1000, arr[0] has 1000 elements, arr[1] has 2000 elements, arr[2] has 3000 elements, etc.
+    int **arr = new int *[nValue];
+    for (int i = 0; i < nSize; i++) {
+        arr[i] = new int[nValue*(i+1)];
+    }
+
+    // fill the array with random numbers
+    for (int i = 0; i < nSize; i++) {
+        int actualSize = nValue * (i + 1);
+        for (int j = 0; j < actualSize; j++) {
+            arr[i][j] = getRandomNumber(0, actualSize);
+        }
+    }
+
+    for(int i = 0; i < nSize; i++) {
+        int actualSize = nValue * (i + 1);
+        int duration = Sort::Insertion(arr[i], actualSize);
+        std::cout << "duration for n = "<< i+1 <<": " << duration << "ms" << std::endl;
+    }
     return 0;
 }
